0ae42cb3698e64bd5bed2e2b82251eb204ecbf21
Leonel <Leone@hotmail.com>
2023-01-24 05:15:10 -0300

    busqueda lista, falta comprobaciones

--- src/components/SearchPokemon.jsx
+++ src/components/SearchPokemon.jsx
@@ -1,10 +1,45 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../sass/search.scss";
 
 const SearchPokemon = () => {
+    const [namePoke, setNamePoke] = useState("");
+    const [pokemon, setPokemon] = useState({});
+    const navigate = useNavigate();
+
+    const navigation = (id) => {
+        navigate(`/pokemon/${id} `);
+    };
+
+    const getPokemon = async (name) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
+        const data = await res.json();
+
+        setPokemon(data);
+        if (data.id) {
+            navigation(data.id);
+        }
+    };
+
+    const getName = (e) => {
+        e.preventDefault();
+        getPokemon(namePoke.toLowerCase());
+        e.target.reset();
+        setNamePoke("");
+    };
     return (
-        <div className="d-flex justify-content-center mt-4 ms-4 align-items-center ">
-            <input type="text" placeholder="Find Pokemon" />
-            <i className="bi bi-search"></i>
+        <div>
+            <form
+                onSubmit={(e) => getName(e)}
+                className="d-flex justify-content-center mt-4 ms-4 align-items-center "
+            >
+                <input
+                    type="text"
+                    placeholder="Find Pokemon"
+                    onChange={(e) => setNamePoke(e.target.value)}
+                />
+                <i className="bi bi-search"></i>
+            </form>
         </div>
     );
 };
